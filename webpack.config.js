0f43a51e280a35eda69d6ae3d2c573883d25af66
David Ruan <ruanwz@gmail.com>
2015-02-10 14:19:53 +0800

    user node to manage front end files

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    home:  "./fe/ap/home/entry.coffee"
+  },
+  output: {
+    path: path.join(__dirname, 'priv', 'static', 'js'),
+    filename: "[name]-bundle.js",
+    publicPath: "/js/"
+  },
+  module: {
+    loaders: [
+        { test: /\.coffee$/, loader: "coffee-loader" },
+        { test: /\.jsx$/, loader: "jsx-loader?harmony" },
+        { test: /\.cjsx$/, loader: "coffee-jsx-loader" },
+        { test: /\.less$/,   loader: "style-loader!css-loader!less-loader"},
+        { test: /\.(eot|woff)$/,   loader: "url-loader?prefix=font/&limit=5000" },
+    ]
+  },
+  resolve: {
+    alias: {
+      ap:     path.join(__dirname, "fe", "ap"),
+      shared: path.join(__dirname, "fe", "ap", "shared") 
+    }
+               
+  }
+};
+
